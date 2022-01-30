da0e0df38a19c168ea5ea110943c7deae18c7c77
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-01-30 13:44:17 +0100

    installation

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<!--  src/routes/$layout.svelte -->
+<script>
+  import '../tailwind.css'
+//   import '../app.css'
+</script>
+
+<div class="container mx-auto p-12 max-w-5xl">
+    <slot />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<main>
+    <h1 class="text-center text-4xl font-bold text-gray-800 md:text-3xl uppercase">Erp module</h1>
+</main>
